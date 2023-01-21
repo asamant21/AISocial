658e829e9c9de8b0a6b42072b6f609fa5445a547
baskaryan <bagatur@robustintelligence.com>
2023-01-21 15:23:28 -0800

    add insert metadata

--- app/api/endpoints/generate.py
+++ app/api/endpoints/generate.py
@@ -1,10 +1,11 @@
 """Endpoints for generating tweets."""
-from typing import List
+from typing import List, Dict
 
 from fastapi import APIRouter
 
 from app.api import schemas
 from app.config import supabase
+from app.constants import METADATA_PROMPT_TWEET_IDS_KEY
 
 router = APIRouter()
 
@@ -48,9 +49,13 @@ def compute_weights(impressions: List[dict]) -> dict:
     return {i["id"]: i["child_like_count"] for i in impressions}
 
 
-def choose_examples(weights: dict) -> List[str]:
-    return ["x_user: example 1", "y_user: example 2"]
+def choose_examples(weights: dict) -> dict:
+    return {1: "x_user: example 1", 2: "y_user: example 2"}
 
 
-def generate_tweet_from_examples(examples: List[str]) -> dict:
-    return {"content": "foo", "author": "foo"}
+def generate_tweet_from_examples(examples: dict) -> dict:
+    return {
+        "content": "foo",
+        "author": "foo",
+        "metadata": {METADATA_PROMPT_TWEET_IDS_KEY: [1, 2]}
+    }
---
+++ app/constants.py
@@ -0,0 +1,3 @@
+
+
+METADATA_PROMPT_TWEET_IDS_KEY = "prompt_examples_tweet_ids"
