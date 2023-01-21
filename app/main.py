8a56e64165fa00a9313789c413318368b2cd6dc3
baskaryan <bagatur@robustintelligence.com>
2023-01-21 14:27:17 -0800

    supa wip

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
-import json
+""""""
 import os
-from typing import List, Type, Union
 
-from flask import Flask, abort, request
+from flask import Flask
 from flask_cors import CORS
-from flask import jsonify
-import numpy as np
 from supabase import create_client, Client
 
 
@@ -18,11 +15,25 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 
-@app.route('/generate-post  ')
+@app.route('/generate-post')
 def generate_post():
+    user_id = get_user_id()
+    # Returned data schema:
+    # [{'id': 1,
+    #   'created_at': '2023-01-21T22:09:53+00:00',
+    #   'tweet_id': 1,
+    #   'user_id': 1,
+    #   'child_like_count': 1,
+    #   'liked': True}]
+    impressions = supabase.table("Impression").select("*").filter("user_id", "eq", user_id).execute().data
     pass
+
+
+def get_user_id():
+    return 1
 
 
 @app.route('/like-tweet', methods=["POST"])
 def like_tweet():
     pass
+
