80197b1cb758d1d202fe7ef5ebc8e2d5ffa8a14e
baskaryan <bagatur@robustintelligence.com>
2023-01-21 14:55:25 -0800

    gen skeleton

--- app/api/endpoints/generate.py
+++ app/api/endpoints/generate.py
@@ -1,9 +1,11 @@
 """Endpoints for generating tweets."""
 import os
+from typing import List
 
-from api import schemas
 from fastapi import APIRouter
 from supabase import create_client, Client
+
+from api import schemas
 
 router = APIRouter()
 
@@ -19,18 +21,37 @@ def generate():
     return {"id": 1, "content": "foo", "author": "foo"}
 
 
-def generate_post():
-    user_id = get_user_id()
-    # Returned data schema:
+def generate_post(user_id: int) -> dict:
+    # Impression data schema:
     # [{'id': 1,
     #   'created_at': '2023-01-21T22:09:53+00:00',
     #   'tweet_id': 1,
     #   'user_id': 1,
     #   'child_like_count': 1,
     #   'liked': True}]
-    impressions = supabase.table("Impression").select("*").filter("user_id", "eq", user_id).execute().data
-    pass
+    impressions = (
+        supabase.table("Impression").select("*").filter("user_id", "eq", user_id).execute().data
+    )
+    weights = compute_weights(impressions)
+    examples = choose_examples(weights)
+    tweet = generate_tweet_from_examples(examples)
+    # Table data schema:
+    # [{'id': 6,
+    #   'created_at': '2023-01-21T22:54:52.770345+00:00',
+    #   'content': 'insert',
+    #   'author': 'test2',
+    #   'metadata': {}}]
+    insert_resp = supabase.table("Tweet").insert(tweet).execute()
+    return insert_resp.data[0]
 
 
-def get_user_id():
-    pass
+def compute_weights(impressions: List[dict]) -> dict:
+    return {1: 0.2}
+
+
+def choose_examples(weights: dict) -> List[str]:
+    return ["example 1", "example 2"]
+
+
+def generate_tweet_from_examples(examples: List[str]) -> dict:
+    return {"content": "foo", "author": "foo"}
