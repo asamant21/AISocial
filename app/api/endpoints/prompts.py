d82be365baee275f311f1315a770dc5c6211a1dd
Akash Samant <asamant@princeton.edu>
2023-01-21 18:11:50 -0800

    change

--- app/api/endpoints/prompts.py
+++ app/api/endpoints/prompts.py
@@ -1,9 +1,10 @@
 from langchain.prompts import FewShotPromptTemplate, PromptTemplate
 
 prefix = """
-You are provided a list of tweets that a particular Twitter user has liked in the past.
+You are a cultural curator and potential comedian. You are provided a list of tweets that a particular Twitter user has liked in the past. 
+They are varied and can have many different styles, affectations, and cover a range of topics.
 
-Use the guidelines below to create your own tweet.
+Use the guidelines below to create your own tweet from those below.
 
 The tweets are provided in the following JSON Format:
 {{
@@ -12,22 +13,25 @@ The tweets are provided in the following JSON Format:
 }}.
 """
 suffix = """
-Guidelines for Creating Your Own Tweet:
-- You should use a writing tone similar to those in the tweets above eg. funny, informative, etc. Don't limit yourself to these. Be specific.
-- You should pretend to be each of the users described above. Take on their personality.
-- You should write on some of the topics that are used in the tweets above, but don't limit yourself. Talk about new things!
-- Never repeat information in the tweets above. Be original
-- Don't mention dates in the past
-- Be opinionated in what you say. Don't be afraid to say something controversial.
-
-
 Tweet JSON Result Format:
 {
-    "tweet": "the tweet itself",
-    "user": "the username that created the tweet. taken from the list of usernames above.",
+    "tweet": "the tweet itself subject to guidelines above",
+    "user": "@ a twitter username. either made up or taken from the list above",
     "tone": "the tone of the tweet that you wrote",
-    "topic": :topics that you decided to write about"
+    "topic": topics that you decided to write about, can be taken from above."
 }
+
+Guidelines for Creating Your Own Tweet:
+- You should use a writing tone similar to those in the tweets above eg. funny, informative, etc. Don't limit yourself to these. Be specific.
+- You should pretend to be each of the users described above. Take on their personality - take risks, be funny.
+- You should write on some of the topics that are used in the tweets above, but don't limit yourself. Talk about new things.
+- Never repeat information in the tweets above
+- Always experiment.
+- Don't mention dates in the past
+- Be opinionated in what you say. Don't be afraid to say something controversial.
+- If you are including information. Do not take an informative tone. Take a side and explain it.
+- make jokes if using a lighthearted tone
+
 
 Output:"""
 eg_template = """{tweets}"""
