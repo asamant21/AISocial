2a4a41f13523d828c7a21c4d80729b83c4154f96
Sean Sullivan <ssullivan61198@gmail.com>
2023-01-21 18:19:11 -0800

    add bad looking-login page

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,40 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { useRouter } from 'next/router';
+import {
+  createBrowserSupabaseClient,
+  Session
+} from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+
+function MyApp({
+  Component,
+  pageProps
+}: AppProps<{ initialSession: Session }>) {
+  const router = useRouter();
+  const [supabaseClient] = useState(() =>
+    createBrowserSupabaseClient()
+  );
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <button
+        onClick={async () => {
+          await supabaseClient.auth.signOut();
+          router.push('/');
+        }}
+      >
+        Logout
+      </button>
+
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
+
+export default MyApp;
