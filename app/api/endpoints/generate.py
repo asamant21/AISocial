7177b9aa676ad396883aa53a7f6cce286ddb958c
Akash Samant <asamant@princeton.edu>
2023-01-21 16:21:02 -0800

    change

--- app/api/endpoints/generate.py
+++ app/api/endpoints/generate.py
@@ -1,9 +1,15 @@
 """Endpoints for generating tweets."""
 import os
 from typing import List
+import numpy as np
+from datetime import datetime
+import json
+from endpoints.prompts import eg_prompt, prefix, suffix, day_quote
 
 from fastapi import APIRouter
 from supabase import create_client, Client
+from langchain.llms import OpenAI
+
 
 from api import schemas
 
@@ -12,7 +18,6 @@ router = APIRouter()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
-
 
 @router.get("", response_model=schemas.Tweet)
 def generate():
@@ -46,12 +51,66 @@ def generate_post(user_id: int) -> dict:
 
 
 def compute_weights(impressions: List[dict]) -> dict:
-    return {1: 0.2}
+    weight_dict: dict = {}
+    total_weight_sum = sum(
+        [impression["child_like_count"] + (10*int(impression["liked"])) for impression in impressions]
+    )
+    for impression in impressions:
+        curr_sum = impression["child_like_count"] + (10*int(impression["liked"]))
+        curr_weight = float(curr_sum)/total_weight_sum
+        tweet_id = impression["tweet_id"]
+        user_id = impression["user_id"]
+        id_tuple = f"{tweet_id}, {user_id}"
+        weight_dict[id_tuple] = curr_weight
+
+    return weight_dict
+
+
+def convert_examples(examples: List[str]) -> List[str]:
+    tweet_template = """"tweet": {tweet}
+    "user": {user}
+    """
+    tweet_arr = []
+    for example in examples:
+        tweet_id, user_id = example.split(", ")
+        tweet_str = tweet_template.format(tweet=tweet_id, user=user_id)
+        tweet_arr.append(tweet_str)
+    return tweet_arr
 
 
 def choose_examples(weights: dict) -> List[str]:
-    return ["example 1", "example 2"]
+    weight_list = list(weights.items())
+    examples = np.random.choice([weight[0] for weight in weight_list], size=5, p=[weight[1] for weight in weight_list])
+    return convert_examples(examples)
 
 
 def generate_tweet_from_examples(examples: List[str]) -> dict:
-    return {"content": "foo", "author": "foo"}
+    eg = eg_prompt.format(tweets=("\n".join(examples)))
+    day_prefix = day_quote.format(today=str(datetime.today().date()))
+    full_prefix = prefix + f"\n{day_prefix}"
+    full_prompt = full_prefix + eg + suffix
+    llm = OpenAI(temperature=1)
+
+    loaded_dict = {}
+
+    curr_temp = 1
+    for i in range(3):
+        try:
+            llm.temperature = curr_temp
+            gen_tweet = llm(full_prompt)
+            stripped_tweet = gen_tweet.strip("\n ")
+            loaded_dict = json.loads(stripped_tweet)
+            break
+        except:
+            # Lower the temperature, it could be getting the formatting wrong
+            curr_temp /= 2
+            continue
+
+    if len(loaded_dict) == 0:
+        return loaded_dict
+
+    return_dict = {
+        "content": loaded_dict["tweet"],
+        "author": loaded_dict["user"]
+    }
+    return return_dict
