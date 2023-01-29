48ddcd72611ed06fde2b838d04ede7c44f6ed8fa
Akash Samant <asamant@princeton.edu>
2023-01-29 10:46:00 -0800

    change

--- frontend/components/Tweet.tsx
+++ frontend/components/Tweet.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
+import { TwitterShareButton } from "react-share";
+import { TwitterIcon } from "react-share";
 import { Tweet } from "@/lib/types";
 import { useRequestCallback } from "@/lib/api";
 
@@ -41,9 +43,9 @@ const Tweet = ({ tweet }: Props) => {
             </p>
           </div>
         </div>
-        <div className='flex flex-col flex-1'>
+        <div className='flex flex-col flex-1' style={{display: 'flex', justifyContent: 'center'}}>
           <div className='flex items-center justify-between mt-1' onClick={handleToggleLike}>
-            <div className={`text-[${color}] text-sm  flex items-center space-x-2 bg-transparent  hover:bg-slate-100 rounded-3xl hover:bg-opacity-10 p-2 hover:text-sky-400`}>
+            <div className={`text-[${color}] text-sm  flex items-center space-x-2 bg-transparent  hover:bg-slate-100 rounded-3xl hover:bg-opacity-10 p-2 hover:text-sky-400`} style={{margin: 'auto'}}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke={color} fill={liked ? color : "none"} className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
               </svg>
