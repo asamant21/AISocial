33e1253085664c64c7d10f71b02b27241b80fa28
Akash Samant <asamant@princeton.edu>
2023-01-31 00:35:17 -0800

    change

--- frontend/components/Feed.tsx
+++ frontend/components/Feed.tsx
@@ -10,7 +10,9 @@ interface Props {
 const Feed = ({ tweets: tweetList }: Props) => {
   const [tweets, setTweets] = useState(tweetList ?? []);
   const [isLoading, setIsLoading] = useState(false);
+  const [isReset, setIsReset] = useState(false)
   const generateTweet = useRequestCallback('/generate');
+  const regenerateTweet = useRequestCallback('/regenerate');
 
   const handleGenerate = useCallback(() => {
     setIsLoading(true);
@@ -30,9 +32,27 @@ const Feed = ({ tweets: tweetList }: Props) => {
     });
   }, [generateTweet]);
 
+  const handleRegenerate = useCallback(() => {
+    setIsReset(true);
+    regenerateTweet().then(res => {
+      const { author, content, id } = res as { author: string, content: string, id: number };
+      const tweet =  {
+        author: { handle: author },
+        content,
+        id,
+        likes: 0,
+        likedByUser: false,
+      };
+      setIsReset(false);
+      setTweets([tweet]);
+    }).catch(() => {
+      setIsReset(false);
+    });
+  }, [regenerateTweet]);
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex flex-row h-10 items-center justify-center mt-10 my-4">
+      <div className="flex flex-row h-10 items-center justify-center mt-10 my-3">
         {isLoading ? (
           <Oval
             height={30}
@@ -47,13 +67,30 @@ const Feed = ({ tweets: tweetList }: Props) => {
             strokeWidthSecondary={4}
           />
         ) : (
-          <button className="rounded-full bg-cyan-500 py-2 px-4 hover:bg-cyan-700 my-4" onClick={handleGenerate}>Generate</button>
+          <button className="rounded-full bg-cyan-500 py-2 px-4 hover:bg-cyan-700 mt-4" onClick={handleGenerate}>Generate</button>
         )}
       </div>
-
-      <div className="w-[32rem] max-w-full">
-        {tweets.map(tweet => <TweetItem key={tweet.id} tweet={tweet} />)}
-      </div>
+      {tweets.length > 0 ? 
+        <div className="flex flex-row h-10 items-center justify-center">
+          <button className="hover:text-white" style={{color: "#808996", fontSize: "15px"}} onClick={handleRegenerate}>Reset</button>
+        </div> : <></>
+      }
+      {isReset? 
+          <Oval
+              height={30}
+              width={30}
+              color="#06B6D4"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+              ariaLabel='oval-loading'
+              secondaryColor="#0e7490"
+              strokeWidth={4}
+              strokeWidthSecondary={4}
+           />: 
+           <div className="w-[32rem] max-w-full">
+           {tweets.map(tweet => <TweetItem key={tweet.id} tweet={tweet} />)}
+          </div>}
     </div>
   );
 }
