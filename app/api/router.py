e47dd0d33c4b0e3e2cb1bc7daf8f05cbcfdc1e82
Matthew Fastow <55714940+mattfast@users.noreply.github.com>
2023-04-04 09:13:33 -0600

    stuff

--- app/api/router.py
+++ app/api/router.py
@@ -7,6 +7,7 @@ from app.api.endpoints import generate, like, regenerate, retrieve_likes, retrie
 api_router = APIRouter()
 api_router.include_router(generate.router, prefix="/generate", tags=["generate"])
 api_router.include_router(regenerate.router, prefix="/regenerate", tags=["regenerate"])
+api_router.include_router(changeStyle.router, prefix="/changeStyle", tags=["changeStyle"])
 api_router.include_router(like.router, prefix="/like", tags=["like"])
 api_router.include_router(
     retrieve_tweet.router, prefix="/retrieve-tweet", tags=["retrieve tweet"]
